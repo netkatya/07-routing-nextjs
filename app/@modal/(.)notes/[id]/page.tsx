57ac2099fba24b189dd2b757a35d49bbb54129f6
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-08-27 14:25:51 +0100

    modif

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,13 +1,13 @@
 import { fetchNoteById } from "@/lib/api";
 import NotePreviewClient from "./NotePreview.client";
-import { Note } from "@/types/note";
 
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 const Page = async ({ params }: Props) => {
-  const note: Note = await fetchNoteById(params.id);
+  const { id } = await params;
+  const note = await fetchNoteById(id);
 
   return <NotePreviewClient note={note} />;
 };
