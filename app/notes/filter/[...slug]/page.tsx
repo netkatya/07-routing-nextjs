d685ececc64afca7f5416c0ee0c65f41de4a4035
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-08-27 14:20:47 +0100

    modif

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -7,17 +7,14 @@ import NotesClient from "./Notes.client";
 import { fetchNotes } from "@/lib/api";
 
 type Props = {
-  params: { slug?: string[] };
+  params: Promise<{ slug?: string[] }>;
 };
 
 export default async function NotesPage({ params }: Props) {
-  const slugTags = params?.slug ?? [];
+  const resolvedParams = await params;
+  const slug = resolvedParams.slug ?? [];
 
-  const apiTags = slugTags.map(
-    (tag) => tag.charAt(0).toUpperCase() + tag.slice(1).toLowerCase()
-  );
-
-  const tag = apiTags[0];
+  const tag = slug[0] === "all" ? undefined : slug[0];
 
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
